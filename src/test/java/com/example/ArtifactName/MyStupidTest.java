df60064cbdb6c1046583850784a22149c832d4d6
Paul CROS <paul.cros.tech@gmail.com>
2025-04-27 16:42:53 +0200

    Test 2.7

---
+++ src/test/java/com/example/ArtifactName/MyStupidTest.java
@@ -0,0 +1,14 @@
+package com.example.ArtifactName;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MyStupidTest {
+
+    @Test
+    public void test() {
+        System.out.println("True is true");
+        assertTrue(true);
+    }
+}
